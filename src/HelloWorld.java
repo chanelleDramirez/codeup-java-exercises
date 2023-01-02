85855c24d0640436511d675fd0b03b52ceaf63f3
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-02 14:15:01 -0600

    Syntax, Types, and Varibles exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.print("Hello, World!");
-
+        System.out.println("Hello, World!");
+        int myFavoriteNumber;
+        myFavoriteNumber = 13;
+        System.out.println("13");
+        String myString;
+        myString = "Happy New Year!";
+        System.out.print("Happy New Year!");
 
     }
 }
