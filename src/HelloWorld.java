851a2b308647a05a3db568941eb9ff47022c4d43
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-02 15:19:54 -0600

    Completed number 8

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,8 +12,14 @@ myString = "C";
 System.out.println(myString);
 myString = "3.14159";
 System.out.println(myString);
- long myNumber;
+  long myNumber;
          System.out.println(myNumber);
+        long myNumber = 3.14;
+        System.out.println(myNumber);
+long myNumber = 123L;
+        System.out.println(myNumber);
+        long myNumber = 123;
+        System.out.println(myNumber);
 
     }
 }
