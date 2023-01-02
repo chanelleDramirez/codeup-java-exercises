2244c32029551f3e0884b1ada55d0ca4d91c54c7
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-02 16:55:40 -0600

    Console IO exercise

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,7 @@
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        System.out.printf("The value of pi approximately 3.14.");
+        System.out.format("%nThe value of pi approximately 3.14.");
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,14 +12,14 @@ myString = "C";
 System.out.println(myString);
 myString = "3.14159";
 System.out.println(myString);
-  long myNumber;
-         System.out.println(myNumber);
-        long myNumber = 3.14;
-        System.out.println(myNumber);
-long myNumber = 123L;
-        System.out.println(myNumber);
-        long myNumber = 123;
-        System.out.println(myNumber);
+//  long myNumber;
+//         System.out.println(myNumber);
+//        long myNumber = 3.14;
+//        System.out.println(myNumber);
+//long myNumber = 123L;
+//        System.out.println(myNumber);
+//        long myNumber = 123;
+//        System.out.println(myNumber);
 
     }
 }
