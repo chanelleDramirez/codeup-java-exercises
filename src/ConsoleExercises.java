3d5556ad4d30a223891dcb0f432525a8329640ec
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-03 12:14:57 -0600

    Scanner class #3 completed

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,9 +5,9 @@ public class ConsoleExercises {
         double pi = 3.14159;
         System.out.printf("The value of pi approximately %.2f.", pi);
         System.out.format("%nThe value of pi approximately %.2f. .%n", pi);
-       // Explore the Scanner Class
+        // Explore the Scanner Class
 
- //Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
+        //Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
 
         Scanner scanner = new Scanner(System.in);
 
@@ -31,15 +31,28 @@ public class ConsoleExercises {
         String inputOne = "firstColor";
         String nextInput = "secondColor";
         String lastInput = "thirdColor";
-        String userInput1=scanner.next();
-        String userInput2=scanner.next();
-        String userInput3=scanner.next();
+        String userInput1 = scanner.next();
+        String userInput2 = scanner.next();
+        String userInput3 = scanner.next();
 
-        System.out.println("You entered: --> \""+ userInput1 +userInput2 + userInput3 +"\" <--");
+        System.out.println("You entered: --> \"" + userInput1 + userInput2 + userInput3 + "\" <--");
 
-    }
+
 // if you enter one word/color or more than 3 words/colors the console will not run your code.
 
 
+        //Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
+
+
+        Scanner search = new Scanner(System.in);
+
+        {
+            System.out.println("Enter a sentence about the Marvel universe: ");
+            String userInput = search.next();
+
+            System.out.println("You entered: --> \"" + userInput + "\" <--");
+        }
     }
 
+    // When the user types the sentence, the console will only populate the first word.
+}
