d43ed5f1b583547c8e55bdf3de73891a1aa20de1
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-03 11:04:58 -0600

    Completed exercise 1 Console IO

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,7 +1,42 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.printf("The value of pi approximately 3.14.");
-        System.out.format("%nThe value of pi approximately 3.14.");
+        System.out.printf("The value of pi approximately %.2f.", pi);
+        System.out.format("%nThe value of pi approximately %.2f.", pi);
+        //Explore the Scanner Class
+
+// Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        {
+//            int integer;
+//
+//            System.out.println("Enter an integer: ");
+//            int number = scanner.nextInt();
+//
+//            System.out.println("You entered: --> \" " + number + " \" <--");
+//
+//        }
+//
+//
+//
+//        // Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
+//
+//
+//        System.out.println("Enter 3 colors: ");
+//        String inputOne = "firstColor";
+//        String nextInput = "secondColor";
+//        String lastInput = "thirdColor";
+//        String userInput=scanner.next();
+//
+//        System.out.println("You entered: --> \""+userInput+"\" <--");
+//
+//    }
+//
+//
+
     }
 }
