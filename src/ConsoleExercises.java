6621472f60a56ab0517c5b1c1795b3f57cc23755
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-03 11:45:37 -0600

    Scanner class #2 completed

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,7 +4,7 @@ public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
         System.out.printf("The value of pi approximately %.2f.", pi);
-        System.out.format("%nThe value of pi approximately %.2f.", pi);
+        System.out.format("%nThe value of pi approximately %.2f. .%n", pi);
        // Explore the Scanner Class
 
  //Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
@@ -25,19 +25,21 @@ public class ConsoleExercises {
 //
 //
 //        // Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
-//
-//
-//        System.out.println("Enter 3 colors: ");
-//        String inputOne = "firstColor";
-//        String nextInput = "secondColor";
-//        String lastInput = "thirdColor";
-//        String userInput=scanner.next();
-//
-//        System.out.println("You entered: --> \""+userInput+"\" <--");
-//
-//    }
-//
-//
+
+
+        System.out.println("Enter 3 colors: ");
+        String inputOne = "firstColor";
+        String nextInput = "secondColor";
+        String lastInput = "thirdColor";
+        String userInput1=scanner.next();
+        String userInput2=scanner.next();
+        String userInput3=scanner.next();
+
+        System.out.println("You entered: --> \""+ userInput1 +userInput2 + userInput3 +"\" <--");
 
     }
-}
+
+
+
+    }
+
