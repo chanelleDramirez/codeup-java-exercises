aad1b17cfc18172a4ed6eb619c11bb64f8d8e3fd
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-03 11:07:31 -0600

    Scanner Class #1 completed

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,22 +5,23 @@ public class ConsoleExercises {
         double pi = 3.14159;
         System.out.printf("The value of pi approximately %.2f.", pi);
         System.out.format("%nThe value of pi approximately %.2f.", pi);
-        //Explore the Scanner Class
+       // Explore the Scanner Class
 
-// Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
+ //Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
 
-//        Scanner scanner = new Scanner(System.in);
-//
-//        {
-//            int integer;
-//
-//            System.out.println("Enter an integer: ");
-//            int number = scanner.nextInt();
-//
-//            System.out.println("You entered: --> \" " + number + " \" <--");
-//
-//        }
-//
+        Scanner scanner = new Scanner(System.in);
+
+        {
+            int integer;
+
+            System.out.println("Enter an integer: ");
+            int number = scanner.nextInt();
+
+            System.out.println("You entered: --> \" " + number + " \" <--");
+
+        }
+
+        // If anything else other than an integer is entered, an error will occur in the console.
 //
 //
 //        // Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
