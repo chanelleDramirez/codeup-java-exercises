02d0528b934c16a4aceb199d7cb83f4f0c68a485
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-04 11:16:53 -0600

    loops exercise 1 B completed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,11 +3,20 @@ public class ControlFlowExercises {
 
         // Loop Basics
         // A. While
-        int i = 5;
-        while ( i <= 15) {
+       // int i = 5;
+        //while (i <= 15) {
+           // System.out.println(i);
+          //  i++;
+      //  }
+
+
+        // B
+        int i = 0;
+        do {
             System.out.println(i);
-            i++;
-        }
-        }
+            i+= 2;
+        } while (i <= 100);
     }
+}
+
 
