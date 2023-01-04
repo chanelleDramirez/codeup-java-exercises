09fe2dc40d0a1636c8b6e17e2087cbc71689e6e1
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-04 15:19:32 -0600

    Loops exercise bullet 3 1B completed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -11,6 +11,7 @@ public class ControlFlowExercises {
 
 
         // B
+        // increments by 2 to 100
 //        int i = 0;
 //        do {
 //            System.out.println(i);
@@ -19,11 +20,22 @@ public class ControlFlowExercises {
 //    }
 
         // B
-        int i = 100;
+        //decrements by 5 to -10
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+
+
+        // B
+        long i = 2;
         do {
             System.out.println(i);
-            i -= 5;
-        } while (i >= -10);
+            i = i * i;
+        } while (i <= 1000000);
+
+
     }
 }
 
