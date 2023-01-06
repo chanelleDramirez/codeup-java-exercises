f4527c618d48dc6f93aafb5525e183c9969bb91f
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-06 16:45:14 -0600

    Methods exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     public static void main(String[] args) {
-        System.out.println(addition(3,4));
-        System.out.println(subtraction(26,13));
-        System.out.println(multiplication(7,6));
-        System.out.println(division(15,3));
-        System.out.println(modulus(11,3));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(addition(3, 4));
+        System.out.println(subtraction(26, 13));
+        System.out.println(multiplication(7, 6));
+        System.out.println(division(15, 3));
+        System.out.println(modulus(11, 3));
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = scanner.nextInt();
+        System.out.println("You entered: --> \" " + userInput + " \" <--");
+        System.out.println("Number must range between 1 and 10");
+        System.out.print("Enter a number between 1 and 10: ");
+        System.out.println("You entered: --> \" " + userInput + " \" <--");
+        System.out.print("Would you like to continue? ");
+        int numberEntered = scanner.nextInt();
+
     }
-    // Create four separate methods. Each will perform an arithmetic operation:
+    // 1. Create four separate methods. Each will perform an arithmetic operation:
 
     public static int addition(int x, int y) {
         return x + y;
@@ -27,4 +39,35 @@ public class MethodsExercises {
     public static int modulus(int k, int n) {
         return k % n;
     }
+
+
+    //2. Create a method that validates that user input is in a certain range and returns that input as an integer if it is within the given range. If not, prompt the user to input their number again until the input is within range.
+
+
+    public static int getInteger(int min, int max) {
+        int userInput = getInteger(1, 10);
+
+        if (userInput <= 10) {
+            //the thing to do
+        } else if (userInput > 11) {
+            //the thing to do
+
+        }
+
+
+        return userInput;
+    }
+
+
+    //3. Calculate the factorial of a number.
+    public static int getFactorial(int number) {
+        int numberEntered = 1;
+        for(int i = 1; i < 10; i ++ );
+
+    } return
+
+
+
+
+
 }
