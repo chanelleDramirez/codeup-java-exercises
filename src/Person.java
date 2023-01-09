a8175a4fd1e80d7a0f0e850ee4f10f9ccd97198d
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-09 13:32:50 -0600

    objects exercise

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    private String name;
+
+    public String getName() {
+        return String.format("%s", name);
+    }
+
+    public void setName(String name){
+}
+
+
+
+//TODO: return the person's name
+
+
+
+//TODO: change the name field to the passed value
+
+   // public void sayHello(){
+//TODO: print a message to the console using the person's name
