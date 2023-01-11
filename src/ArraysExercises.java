e6ed56565b133b11c75c0253f3991d2050bb5682
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-11 11:29:44 -0600

    Inheritance exercise

---
+++ src/ArraysExercises.java
@@ -0,0 +1,8 @@
+//import java.util.Arrays;
+//public class ArraysExercises {
+//    public static void main(String[] args) {
+//        Arrays.toString
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers);
+//    }
+//}
--- src/Person.java
+++ src/Person.java
@@ -6,6 +6,21 @@ public class Person {
     }
 
     public void setName(String name){
+        this.name = name;
+    }
+    public void sayHello(){
+        System.out.printf("Hello, from %s.%n", name);
+    }
+    public Person(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Person adrian = new Person("Person adrian");
+        adrian.sayHello();
+        adrian.setName("Adrian Ramirez");
+        System.out.println(adrian.getName());
+    }
 }
 
 
@@ -16,5 +31,5 @@ public class Person {
 
 //TODO: change the name field to the passed value
 
-   // public void sayHello(){
+
 //TODO: print a message to the console using the person's name
