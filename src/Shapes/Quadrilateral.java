ca7d37edda5ad2205264516cf91a6b927d0b349a
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-11 15:49:46 -0600

    Abstract exercise

---
+++ src/Shapes/Quadrilateral.java
@@ -0,0 +1,10 @@
+package Shapes;
+
+public abstract class Quadrilateral extends Shape implements Measurable{
+    protected int length ;
+    protected int width;
+    public Quadrilateral(int length, int width){
+         this.length = length;
+                  this.width = width;
+    }
+}
--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -3,27 +3,5 @@ package Shapes;
 
 public class Rectangle {
 
-    protected double length;
-    protected double width;
-
-
-    public Rectangle(double length, double width) {
-         this.length = length;
-        this.width = width;
-    }
-
-public double getArea(){
-    return length * width;
-}
-public double getPerimeter() {
-        return (2 * length) + (2 * width);
-    }
-
-    public static void main(String[] args) {
-Rectangle box1 = new Rectangle(5,4);
-        System.out.println(box1.length);
-        System.out.println(box1.getArea());
-        System.out.println(box1.getPerimeter());
-    }
 }
 
