7ea7c0b33c97da9ef8b5ab797e6ddd9fe02f2fbb
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-13 17:37:29 -0600

    Objects exercise

---
+++ src/util/Input.java
@@ -0,0 +1,52 @@
+package util;
+import java.util.Scanner;
+
+public class Input {
+
+
+    Scanner scanner;
+
+    public Input() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+      return scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        String response = scanner.nextLine();
+        if (response.equals("yes") || response.equals("y")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public int getInt(int min, int max) {
+        int response = scanner.nextInt();
+            if(response <= max && response >= min ){
+                return response;
+            }else{
+
+                return getInt(min,max);
+    }
+
+
+    }
+    public double getDouble(double min, double max){
+        double response = scanner.nextDouble();
+        if(response <= max && response >= min ){
+            return response;
+        }else {
+
+            return getDouble(min, max);
+        }
+    }
+    public int getInt(){
+        return scanner.nextInt();
+    }
+
+    public double getDouble(){
+        return scanner.nextDouble();
+    }
+}
