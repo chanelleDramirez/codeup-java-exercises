5d09c72e8c740b81253e433004f1a75fb8ca84ad
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-02-01 16:42:55 -0600

    Inheritance and Poly (Rectangle)

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -1,27 +1,27 @@
-//package Shapes;
-//
-//
-//public class Rectangle {
-//protected  int length;
-//protected int width;
-//public Rectangle (int length, int width){
-//   int length = length;
-//   int width = width;
-//}
-//public Rectangle(int length, int width{
-//    int length = 10 + 10;
-//    int width = 5 + 5;
-//}
-//public static getPerimeter(length, width){
-//    perimeter = 2 x length + 2 x width;
-//            return
-//}
-//    public static getArea(length, width){
-//        area = length x width;
-//        return
-//    }
-//}
-//
-//
-//
-//
+package Shapes;
+
+public class Rectangle {
+    protected int length;
+    protected int width;
+
+    protected int side;
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+
+        public Rectangle(int length, int width) {
+            this.length = 10 + 10;
+            this.width = 5 + 5;
+        }
+        public static getPerimeter(int length, int width) {
+            perimeter = 2 * length + 2 * width;
+            return ((2 * length) + (2 * width)
+        }
+        public static getArea(int length, int width) {
+            area = length * width;
+            return( width * length)
+        }
+    }
+
