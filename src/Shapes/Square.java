1f45439f8c8fd491c22d0861309cfc434f4028ef
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-02-01 16:48:52 -0600

    Inheritance and Poly (Square)

--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -1,5 +1,19 @@
 package Shapes;
 
-public class Square {
+public class Square extends Rectangle {
 
-}
+    public Square(int side) {
+        super(side, side);
+        this.side = side;
+    }
+
+        public int getArea(){
+            return (side * side)
+            }
+
+            public int getPerimeter() {
+        return 4 * side;
+            }
+
+    }
+
