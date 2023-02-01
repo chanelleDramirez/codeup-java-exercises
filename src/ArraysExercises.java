1c532941eb2a6910592360c279ab6c313e6f5643
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-02-01 15:59:01 -0600

    Arrays exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,8 +1,20 @@
-//import java.util.Arrays;
-//public class ArraysExercises {
-//    public static void main(String[] args) {
-//        Arrays.toString
-//        int[] numbers = {1, 2, 3, 4, 5};
-//        System.out.println(numbers);
-//    }
-//}
+import java.util.Arrays;
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+        Person [] boy = new Person[3];
+        boy[0] = new Person("Khalil");
+        boy[1] = new Person("Diego");
+        boy[2] = new Person("Cruz");
+        for(Person person: boy){
+            System.out.println(person.getName());
+        }
+    }
+    public static Person[] addPerson(){
+        Person[] boy = Arrays.addPerson(array.length + 1++);
+        boy[array.length] = new Person;
+        return boy;
+    }
+}
+
---
+++ src/Shapes/Circle.java
@@ -0,0 +1,21 @@
+//package Shapes;
+//
+//public class Circle {
+//
+//    private int radius;
+//
+//    public Circle(int radius){
+//        radius = 6;
+//    }
+//
+//    public Circle(double radius){
+//        radius = 6
+//        return double radius;
+//    }
+//    public double getArea(){
+//
+//    }
+//
+//    public double getCircumference()
+//
+//}
